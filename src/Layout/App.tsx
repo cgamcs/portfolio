7f72982cddaedfa04780e042aff419e1eef7a9e2
cgamcs <cesar.garciamcs@proton.me>
2026-03-05 22:09:28 -0600

    arreglando gap en experiencia y haciendo responsiva el tamano de fuente

--- src/Layout/App.tsx
+++ src/Layout/App.tsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer"
 
 export default function App() {
   return (
-    <div className="w-11/12 md:max-w-3xl lg:max-w-2xl mx-auto text-[#63635E] dark:text-[#dbdbdb] text-lg">
+    <div className="w-11/12 md:max-w-3xl lg:max-w-2xl mx-auto text-[#63635E] dark:text-[#dbdbdb] text-base md:text-lg">
       <Header />
 
       <main>
